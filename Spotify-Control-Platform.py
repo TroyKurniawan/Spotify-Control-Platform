8df677a513a2f47434afc70a3901b64983cd425f
Troy Kurniawan <troy.kurniawan@sjsu.edu>
2018-04-30 16:22:25 -0700

    List all songs by an artist

--- Spotify-Control-Platform.py
+++ Spotify-Control-Platform.py
@@ -22,13 +22,14 @@ from json.decoder import JSONDecodeError
 
 #   Get user's ID from the terminal
 userID = sys.argv[1]
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
 #   Erase cache and prompt for user permission:
 try:
-    token = util.prompt_for_user_token(userID)
+    token = util.prompt_for_user_token(userID, scope)
 except:
     os.remove(f".cache-{userID}")
-    token = util.prompt_for_user_token(userID)
+    token = util.prompt_for_user_token(userID, scope)
 
 # ===============================================================================================
 
@@ -36,6 +37,22 @@ except:
 spotifyObject = spotipy.Spotify(auth=token)
 user = spotifyObject.current_user()
 displayName = user['display_name']
+
+# Get device
+# devices = spotifyObject.devices()
+# print(json.dumps(devices, sort_keys=True, indent=4))
+# print()
+# deviceID = devices['devices'][0]['id']
+
+# Current track playing
+# currentPlaying = spotifyObject.current_user_playing_track()
+# print(json.dumps(devices, sort_keys=True, indent=4))
+# print()
+# artist = currentPlaying['item']['artists'][0]['name']
+# currentPlaying = currentPlaying['item']['name']
+
+# if artist != "":
+#     print("Now Playing: " + artist + " - " + currentPlaying)
 
 print("========================================")
 print("======= Spotify Control Platform =======")
@@ -49,12 +66,13 @@ while True:
     print()
     print("What would you like to do?")
     print("1. Serach for an artist")
-    print("2.")
-    print("3.")
+    print("2. Search for a song")
+    print("3. ")
     print("4. Exit")
     print()
     userInput = input(">>> Enter a number: ")
 
+    # Serach for an artist
     if userInput == "1":
         print()
         search = input(">>> Enter the artist's alias: ")
@@ -64,16 +82,59 @@ while True:
         result = spotifyObject.search(search,1,0,"artist")
         artist = result['artists']['items'][0]
         artistID = artist['id']
-        print("Now accessing: " + artist['name'])
+        album = spotifyObject.artist_albums(artistID)
+        album = artist['id']
 
         # Opening image in browser
         webbrowser.open(artist['images'][0]['url'])
+
+        # Album details
+        trackURI = []
+        trackArt = []
+        i = 0
+        album = spotifyObject.artist_albums(artistID)
+        album = album['items']
+
+        print("Now accessing: " + artist['name'])
         print()
 
-        print("What would you like to do?")
-        print("1. List top 10 songs")
-        print()
-        userInput = input(">>> Enter a number: ")
+        for item in album:
+            print("[ ALBUM ] " + item['name'])
+            albumID = item['id']
+            albumArt = item['images'][0]['url']
+            tracks = spotifyObject.album_tracks(albumID)
+            tracks = tracks['items']
+
+            for item in tracks:
+                print(str(i) + ": " + item['name'])
+                trackURI.append(item['uri'])
+                trackArt.append(albumArt)
+                i+=1
+            print()
+
+        while True:
+            # Artist menu
+            print()
+            print("What would you like to do for this artist?")
+            print("1. Play a song")
+            print("1. List top 10 songs (by play count)")
+            print("2. List all albums")
+            print("3. Exit")
+            print()
+            userInput = input(">>> Enter a number: ")
+
+            # Play a song
+            if userInput == "1":
+                print()
+
+            # List top 10 songs (by play count)
+            if userInput == "1":
+                print()
+
+            elif userInput == "4":
+                print()
+                break
+        
         
 
     elif userInput == "2":
